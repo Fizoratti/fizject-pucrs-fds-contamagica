46a292d8251244ff96aac391446f8eeae1b7165e
fizoratti <robson.felipe@acad.pucrs.br>
2019-03-19 20:04:26 -0300

    Create Conta Magica

---
+++ src/br/pucrs/ep/es/ContaMagica.java
@@ -0,0 +1,12 @@
+package br.pucrs.ep.es;
+
+public class ContaMagica {
+    public ContaMagica(String nome) {
+
+    }
+//        public String getNomeCliente(){…}
+//        public BigDecimal getSaldo(){…}
+//        public Categorias getStatus(){…}
+//        public void deposito(BigDecimal valor) {…}
+//        public void retirada(BigDecimal valor) {…}
+}
