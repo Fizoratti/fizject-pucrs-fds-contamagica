14d4944932a10694bd153ad88517f4873ad1c4af
fizoratti <robson.felipe@acad.pucrs.br>
2019-03-19 20:12:12 -0300

    Add idade to Cliente

--- src/br/pucrs/ep/es/Cliente.java
+++ src/br/pucrs/ep/es/Cliente.java
@@ -2,15 +2,26 @@ package br.pucrs.ep.es;
 
 public class Cliente {
     public String nome;
+    public int idade;
 
     public Cliente(String nome) {
         setNome(nome);
     }
+    public Cliente(String nome, int idade) {
+        setNome(nome);
+        setIdade(idade);
+    }
 
+    public String getNome() {
+        return this.nome;
+    }
     public void setNome(String nome) {
         this.nome = nome;
     }
-    public String getNome() {
-        return this.nome;
+    public int getIdade() {
+        return this.idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 }
