8166ba10f3ca001e2f143341a464168d67145b67
fizoratti <robson.felipe@acad.pucrs.br>
2019-03-19 20:50:46 -0300

    Add saldo & status to ContaMagica

--- src/br/pucrs/ep/es/Conta.java
+++ src/br/pucrs/ep/es/Conta.java
@@ -2,6 +2,7 @@ package br.pucrs.ep.es;
 
 public class Conta {
     private Cliente cliente;
+
     public Conta(Cliente cliente) {
         setCliente(cliente);
     }
--- src/br/pucrs/ep/es/ContaMagica.java
+++ src/br/pucrs/ep/es/ContaMagica.java
@@ -1,14 +1,23 @@
 package br.pucrs.ep.es;
 
-public class ContaMagica extends Conta {
+import java.math.BigDecimal;
 
+public class ContaMagica extends Conta {
+    private BigDecimal saldo;
+    private Categoria status;
 
     public ContaMagica(Cliente cliente) {
         super(cliente);
+        this.saldo = new BigDecimal("0.0");
+        this.status = Categoria.SILVER;
     }
 
-//    public BigDecimal getSaldo(){…}
-//    public Categorias getStatus(){…}
+    public BigDecimal getSaldo(){
+        return this.saldo;
+    }
+    public Categoria getStatus(){
+        return this.status;
+    }
 //    public void deposito(BigDecimal valor) {…}
 //    public void retirada(BigDecimal valor) {…}
 }
